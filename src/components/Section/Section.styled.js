1a5c8a10b164b5db9dcf713db2a85fcb177b478d
Vasylyna Shvets <104644733+Weselka@users.noreply.github.com>
2022-12-06 19:23:06 +0200

    add global style

---
+++ src/components/Section/Section.styled.js
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled';
+
+export const Section = styled.section`
+  // background: ${props => props.theme.colors.light};
+  padding-top: ${props => props.theme.spacing(20)};
+  padding-bottom: ${props => props.theme.spacing(20)};
+`;
