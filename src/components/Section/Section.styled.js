d4f7b6ae2913573952a89c2c76133bd3bfcbcb17
Vasylyna Shvets <104644733+Weselka@users.noreply.github.com>
2022-12-09 20:51:09 +0200

    changes global style

--- src/components/Section/Section.styled.js
+++ src/components/Section/Section.styled.js
@@ -5,3 +5,7 @@ export const Section = styled.section`
   padding-top: ${props => props.theme.spacing(20)};
   padding-bottom: ${props => props.theme.spacing(20)};
 `;
+
+export const SectionTitle = styled.h2`
+  margin-bottom: 12px;
+`;
