7588fc111c3cf1c3289ebaeb9c7d5c88d819de3b
Vasylyna Shvets <104644733+Weselka@users.noreply.github.com>
2022-12-10 13:48:40 +0200

    add component Notification

---
+++ src/components/Notification/Notification.styled.js
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const Message = styled.p`
+  color: ${props => props.theme.colors.gray};
+  //   font-weight: 900;
+  font-size: ${props => props.theme.fontSizes.medium};
+  letter-spacing: ${props => props.theme.spacing(0.25)};
+  margin-left: ${props => props.theme.spacing(2)};
+  margin-bottom: 0;
+`;
