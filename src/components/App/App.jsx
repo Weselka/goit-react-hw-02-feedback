0d8fa2ed9c569d6f8143e21c52281849fab0bfc5
Vasylyna Shvets <104644733+Weselka@users.noreply.github.com>
2022-12-10 14:54:36 +0200

    refactoring App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,72 @@
-import { Heading } from 'components';
-import {Feedback} from 'components';
+import { Component } from 'react';
 
-export const App = () => {
-  return (
-    <>
-      <Heading marginBottom="50px" textAlign="center">
-        Cafe "Expresso"
-      </Heading>
-      <Feedback />
-    </>
-  );
-};
+import { Heading } from 'components';
+import { Container, Section } from 'components';
+import { FeedbackOptions } from 'components';
+import { Notification } from 'components';
+import { Statistics } from 'components';
+
+export class App extends Component {
+  // static defoultProps = {};
+  // static propTypes = {};
+
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onLeaveFeedback = optionState => {
+    this.setState(prevState => {
+      return {
+        [optionState]: prevState[optionState] + 1,
+      };
+    });
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good, neutral, bad } = this.state;
+    return Math.round((good / (good + neutral + bad)) * 100);
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const {
+      state,
+      onLeaveFeedback,
+      countTotalFeedback,
+      countPositiveFeedbackPercentage,
+    } = this;
+    return (
+      <Container>
+        <Heading marginBottom="50px" textAlign="center">
+          Cafe "Expresso"
+        </Heading>
+        <Section title={'Please leave feedback'}>
+          <FeedbackOptions
+            options={Object.keys(state)}
+            onLeaveFeedback={onLeaveFeedback}
+          />
+        </Section>
+        <Section title={'Statistics'}>
+          {countTotalFeedback() === 0 ? (
+            <Notification message="There is no feedback"></Notification>
+          ) : (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={countTotalFeedback()}
+              positivePercentage={countPositiveFeedbackPercentage()}
+            />
+          )}
+        </Section>
+      </Container>
+    );
+  }
+}
--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,6 @@ export * from 'components/Container/Container.styled.js';
 export * from 'components/Section/Section.js';
 export * from 'components/Section/Section.styled.js';
 export * from 'components/Heading/Heading.styled.js';
-export * from 'components/Feedback/Feedback.js';
 export * from 'components/FeedbackOptions/FeedbackOptions.js';
 export * from 'components/Statistics/Statistics.js';
 export * from 'components/Notification/Notification.js';
